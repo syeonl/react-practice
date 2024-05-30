1de6e76bc5b2ec6ff5e11d33556c13ec537b5c11
syeonl <dltmddus8931@naver.com>
2024-05-30 14:15:35 +0900

    update

--- src/context/ThemeModeButton.js
+++ src/context/ThemeModeButton.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
+// import React from 'react';
+// import styled, { css } from 'styled-components';
 
-const ThemeModeButton = ({ toggleTheme, themeMode }) => {
-  const theme = themeMode === 'lightTheme' ? lightTheme : darkTheme;
+// const ThemeModeButton = ({ toggleTheme, themeMode }) => {
+//   const theme = themeMode === 'lightTheme' ? lightTheme : darkTheme;
 
-  const toggleTheme = () => {
-    if (themeMode === 'lightTheme') {
-      setThemeMode('darkTheme');
-      window.localStorage.setItem('theme', 'darkTheme');
-    } else {
-      setThemeMode('lightTheme');
-      window.localStorage.setItem('theme', 'lightTheme');
-    }
-  };
-  return (
-    <ThemeModeWrapper onClick={toggleTheme}>
-      {themeMode === 'lightTheme' ? 'light' : 'dark'}
-    </ThemeModeWrapper>
-  );
-};
+//   const toggleTheme = () => {
+//     if (themeMode === 'lightTheme') {
+//       setThemeMode('darkTheme');
+//       window.localStorage.setItem('theme', 'darkTheme');
+//     } else {
+//       setThemeMode('lightTheme');
+//       window.localStorage.setItem('theme', 'lightTheme');
+//     }
+//   };
+//   return (
+//     <ThemeModeWrapper onClick={toggleTheme}>
+//       {themeMode === 'lightTheme' ? 'light' : 'dark'}
+//     </ThemeModeWrapper>
+//   );
+// };
 
-export const ThemeModeWrapper = styled.button`
-  position: absolute;
-  top: 0;
-  right: 0;
+// export const ThemeModeWrapper = styled.button`
+//   position: absolute;
+//   top: 0;
+//   right: 0;
 
-  width: 80px;
-  margin: 10px;
-  border: none;
-  border-radius: 10px;
+//   width: 80px;
+//   margin: 10px;
+//   border: none;
+//   border-radius: 10px;
 
-  ${({ theme }) => {
-    return css`
-      background-color: ${(props) => props.theme.colors.colorMain};
-      box-shadow: ${(props) => props.theme.colors.colorShadow};
-    `;
-  }}
-`;
+//   ${({ theme }) => {
+//     return css`
+//       background-color: ${(props) => props.theme.colors.colorMain};
+//       box-shadow: ${(props) => props.theme.colors.colorShadow};
+//     `;
+//   }}
+// `;
 
-export default ThemeModeButton;
+// export default ThemeModeButton;
