8346c519cac42e21e5a4b45a87a13bdda1939c24
syeonl <dltmddus8931@naver.com>
2024-05-30 14:23:40 +0900

    code updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import { useState } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from 'context/ThemeContext';
+// import { useState } from 'react';
+// import { ThemeProvider } from 'styled-components';
+// import { darkTheme, lightTheme } from 'context/ThemeContext';
 
 function App() {
-  const [themeMode, setThemeMode] = useState(lightTheme);
-  const theme = themeMode === 'lightTheme' ? lightTheme : darkTheme;
+  // const [themeMode, setThemeMode] = useState(lightTheme);
+  // const theme = themeMode === 'lightTheme' ? lightTheme : darkTheme;
 
   return (
-    <ThemeProvider theme={theme}>
+    // <ThemeProvider theme={theme}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Home themeMode={themeMode} />}></Route>
+        {/* <Route path="/" element={<Home themeMode={themeMode} />}></Route> */}
+        <Route path="/" element={<Home />}></Route>
       </Routes>
-    </ThemeProvider>
+    </BrowserRouter>
+    // </ThemeProvider>
   );
 }
 
